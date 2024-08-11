32f6a9832dfaa8a3f16bba14455d99063b31e8a8
John Pazarzis <jpazarzis@gmail.com>
2024-08-11 18:30:02 -0400

    Implements the save_chunks_to_db and its tests.

--- mygenai/libs/chunks_mgr.py
+++ mygenai/libs/chunks_mgr.py
@@ -1,21 +1,39 @@
 """Document Manager (Manages the document storage)."""
 
+import json
 import os
 
 import mygenai.libs.common as common
 import mygenai.libs.dbutil as dbutil
-import mygenai.libs.impl.doc_splitter_impl as doc_splitter_impl
+import mygenai.libs.impl.splitter as splitter
 
 
-def build_chunks(fullpath, chunk_size=500, chunk_overlap=40):
+@common.handle_exceptions
+def save_chunks_to_db(db, fullpath, chunk_size=500, chunk_overlap=40):
     """Splits the passed in document and saves the chunks into the database.
 
+    :param SimpleSQL db: The database wrapper to use.
     :param str fullpath: The fullpath to the document.
     :param int chunk_size: The chunk size to use.
     :param int chunk_overlap: The chunk overlap The overlap to use.
     """
+    assert os.path.isfile(fullpath)
+    chunk_index = 0
+    for chunk, metadata in splitter.split(fullpath, chunk_size, chunk_overlap):
+        chunk = chunk.replace("'", "''")
+        chunk_index += 1
+        meta = json.dumps(metadata)
+        sql = _SQL_INSERT_CHUNK.format(
+            filepath=fullpath,
+            chunk_index=chunk_index,
+            txt=chunk,
+            meta=meta
+        )
+        print(sql)
+        db.execute_non_query(sql)
 
 
+@common.handle_exceptions
 def find_all_documents(directory):
     """Discovers all the documents under the given directory.
 
@@ -24,7 +42,7 @@ def find_all_documents(directory):
     :return: A list of strings holding the full paths of the documents.
     :rtype: list[str]
     """
-    extensions = doc_splitter_impl.get_supported_doc_extensions()
+    extensions = splitter.get_supported_doc_extensions()
     matches = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -59,6 +77,12 @@ _SQL_SELECT_FULLPATHS = """
 Select fullpath from chunks group by fullpath
 """
 
+_SQL_INSERT_CHUNK = """
+Insert into chunks (fullpath, chunk_index, chunk, metadata) 
+values ('{filepath}', {chunk_index}, '{txt}', '{meta}')
+"""
+
+
 def _get_already_chunked_files():
     """Returns a list with the files that are already chunked and stored in db.
 
@@ -70,6 +94,3 @@ def _get_already_chunked_files():
         for row in db.execute_query(_SQL_SELECT_FULLPATHS):
             fullpaths.append(row[0])
     return fullpaths
-
-
-
--- mygenai/libs/tests/test_chunks_mgr.py
+++ mygenai/libs/tests/test_chunks_mgr.py
@@ -8,9 +8,14 @@ import mygenai.libs.chunks_mgr as chunks_mgr
 
 
 class TestModule(unittest.TestCase):
-    """Tests."""
+    """Tests the chunks_mgr module."""
 
     _DB_NAME = "dummy"
+    _SQL_CLEAR_CHUNKS = "DElETE FROM chunks"
+
+    def tearDown(self):
+        """Cleans up the environment upon finishing a test."""
+        dbutil.SimpleSQL.register_connection_string(None)
 
     def test_find_all_documents(self):
         """Tests the find_all_documents module."""
@@ -34,14 +39,28 @@ class TestModule(unittest.TestCase):
         the find_documents_to_chunk should return the same list of
         documents (since none of them is already processed).
         """
-        conn_str = common.make_local_connection_string("dummy")
+        conn_str = common.make_local_connection_string(self._DB_NAME)
         dbutil.SimpleSQL.register_connection_string(conn_str)
         with dbutil.SimpleSQL() as db:
-            db.execute_non_query("delete from chunks")
+            db.execute_non_query(self._SQL_CLEAR_CHUNKS)
             directory = common.get_testing_data_directory()
             retrieved = chunks_mgr.find_documents_to_chunk(directory)
             expected = chunks_mgr.find_all_documents(
                 common.get_testing_data_directory()
             )
             self.assertListEqual(sorted(expected), sorted(retrieved))
-        dbutil.SimpleSQL.register_connection_string(None)
+
+    def test_save_chunks_to_db(self):
+        """Saves the chunks for a given file to the database."""
+        conn_str = common.make_local_connection_string(self._DB_NAME)
+        dbutil.SimpleSQL.register_connection_string(conn_str)
+
+        # Verify that there are not chunks in the database.
+        with dbutil.SimpleSQL() as db:
+            db.execute_non_query(self._SQL_CLEAR_CHUNKS)
+
+            directory = common.get_testing_data_directory()
+            docs_to_chunk = chunks_mgr.find_documents_to_chunk(directory)
+
+            for fullpath in docs_to_chunk:
+                chunks_mgr.save_chunks_to_db(db, fullpath)
