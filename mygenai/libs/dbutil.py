535b869c9709d23253a3a0a664a6b2a7c9bee825
John Pazarzis <jpazarzis@gmail.com>
2024-08-11 11:23:37 -0400

    Refactors dbutil.

--- mygenai/libs/dbutil.py
+++ mygenai/libs/dbutil.py
@@ -4,22 +4,48 @@ import psycopg2
 
 
 class SimpleSQL:
+    """Provides a simplified interface for interacting with PostgreSQL."""
+
     _connection_string = None
 
     @classmethod
     def register_connection_string(cls, connection_string):
+        """Registers the connection string for subsequent database interactions.
+
+        :param connection_string: The connection string to the database.
+        """
         cls._connection_string = connection_string
 
     def __enter__(self):
+        """Establishes a database connection when entering a with block.
+
+        : return: The SimpleSQL instance itself.
+
+        :raises psycopg2.DatabaseError: If an error occurs during execution.
+        """
         self._connection = psycopg2.connect(self._connection_string)
         return self
 
     def __exit__(self, exc_type, exc_value, trace):
+        """Closes the database connection when exiting a with block.
+
+        :param exc_type: The exception type.
+        :param exc_value: The exception value.
+        :param trace: The exception traceback.
+        """
         assert self._connection
         self._connection.close()
         self._connection = None
 
     def execute_query(self, sql):
+        """Executes a query and yields the results row by row.
+
+        :param sql: The SQL SELECT statement to execute.
+
+        :yield: A tuple representing each row of the query result.
+
+        :raises psycopg2.DatabaseError: If an error occurs during execution.
+        """
         assert self._connection
         with self._connection.cursor() as cursor:
             cursor.execute(sql)
