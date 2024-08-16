58f71a9a9c7fbe14432285bf2fcd8335ed5d5f3f
John Pazarzis <jpazarzis@gmail.com>
2024-08-16 06:23:47 -0400

    Adds the get_testing_output_dir to common.py

--- mygenai/libs/common.py
+++ mygenai/libs/common.py
@@ -3,6 +3,7 @@
 import json
 import os
 import pathlib
+import shutil
 
 _CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 _TESTING_DATA_DIR = os.path.join(_CURRENT_DIR, "testing_data")
@@ -27,6 +28,55 @@ def get_home_dir():
     :rtype: str
     """
     return pathlib.Path.home()
+
+
+def get_testing_output_dir(relative_path, wipe_out=True):
+    """Returns a directory containing temporary output used for testing.
+
+    As an example of what can go to this directory you can think of a
+    vector database that is used for testing purposes or whatever else
+    that might not be need to be stored.
+
+    :param str relative_path: The path that is relative to the top level
+    testing directory.
+
+    :param bool wipe_out: If True, then whatever already exists under
+    the relative path will be deleted and it will start with a clean state.
+
+    :return: A string pointing to a directory containing testing output.
+    :rtype: str
+
+    :raises NotADirectoryError: the passed in relative_path is pointing
+    to an exising file (not a directory).
+
+    :raises: OSError: OS related error when creating or deleting the directory.
+    """
+    output_dir = os.path.join(get_home_dir(), "testing_output")
+
+    # If pointing to a file raise an exception.
+    if os.path.isfile(output_dir):
+        raise NotADirectoryError
+
+    # If the base output directory does not exist then create it.
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    fullpath = os.path.join(output_dir, relative_path)
+
+    # If pointing to a file raise an exception.
+    if os.path.isfile(fullpath):
+        raise NotADirectoryError
+
+    # If the directory already exists delete it if wipe_out is set.
+    if os.path.isdir(fullpath) and wipe_out:
+        shutil.rmtree(fullpath)
+        os.makedirs(fullpath)
+
+    # If the output directory does not exist then create it.
+    if not os.path.isdir(fullpath):
+        os.makedirs(fullpath)
+
+    return fullpath
 
 
 def init_settings():
