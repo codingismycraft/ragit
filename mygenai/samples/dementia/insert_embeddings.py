11150bb3e72d57d35447a5fd81d8bb111fc027cc
John Pazarzis <jpazarzis@gmail.com>
2024-08-17 15:37:50 -0400

    Uses rag manager from dementia.insert_embeddings.

--- mygenai/samples/dementia/insert_embeddings.py
+++ mygenai/samples/dementia/insert_embeddings.py
@@ -4,32 +4,32 @@ import time
 
 import mygenai.libs.common as common
 import mygenai.libs.dbutil as dbutil
-import mygenai.libs.chunks_mgr as chunks_mgr
+import mygenai.libs.rag_mgr as rag_mgr
 import mygenai.samples.dementia.settings as settings
 
 
-def main(max_length=2000):
+def insert_embeddings(max_length=2000):
     """Inserts the chunks to the database.
 
     :param max_length: The number of chunks to calculate embeddings for.
     """
+    common.init_settings()
     conn_str = common.make_local_connection_string(settings.DBNAME)
     dbutil.SimpleSQL.register_connection_string(conn_str)
-
-    counter = 0
+    ragger = rag_mgr.RagManager(settings.RAG_COLLECTION_NAME)
     with dbutil.SimpleSQL() as db:
-        for chunk_id in chunks_mgr.find_chunks_missing_embeddings(db):
-            counter += 1
-            if counter > max_length:
-                break
-            chunks_mgr.save_embeddings(db, chunk_id)
-            print(counter, chunk_id)
+        inserted = ragger.insert_embeddings_to_db(
+            db, max_count=max_length, verbose=True
+        )
+    return inserted
 
 
 if __name__ == '__main__':
-    common.init_settings()
-    c = 0
-    while c < 50:
-        main()
+    total_counter = 0
+    while True:
+        insert_count = insert_embeddings()
+        total_counter += insert_count
+        if insert_count == 0:
+            print(f"done inserting: {total_counter} embeddings.")
+            break
         time.sleep(30)
-        c += 1
