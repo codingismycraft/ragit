c4136333978a6006f8b06f9cdea77aaba7f439a2
John Pazarzis <jpazarzis@gmail.com>
2024-08-17 11:56:00 -0400

    Adds the stored_in_vdb field in the database.

--- mygenai/db/create_schema.sql
+++ mygenai/db/create_schema.sql
@@ -6,5 +6,8 @@ CREATE TABLE chunks
     chunk         TEXT NOT NULL,
     embeddings    jsonb default NULL,
     metadata      jsonb default NULL,
+    stored_in_vdb INTEGER NOT NULL default 0,
     UNIQUE (fullpath, chunk_index)
 );
+
+CREATE INDEX  idx_stored_in_vdb ON chunks(stored_in_vdb);
