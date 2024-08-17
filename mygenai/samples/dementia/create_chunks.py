14c5bac46817a720ef818d234074fdf53c780f7a
John Pazarzis <jpazarzis@gmail.com>
2024-08-17 15:26:52 -0400

    Uses rag manager from dementia.create_chunks.

--- mygenai/samples/dementia/create_chunks.py
+++ mygenai/samples/dementia/create_chunks.py
@@ -1,28 +1,29 @@
-"""Sample application using the dementia related papers.
+"""Inserts the chunks for new documents added to collection.
 
-Assumes that we have a db named dementia available.
+Assumes that we have a db named dementia available. If not you can
+create one by running the mygenai/db/create_db.sh utility:
+
+    cd mygenai/db
+    ./create_db.sh dementia
 """
-
-import datetime
 
 import mygenai.libs.common as common
 import mygenai.libs.dbutil as dbutil
-import mygenai.libs.chunks_mgr as chunks_mgr
+import mygenai.libs.rag_mgr as rag_mgr
 import mygenai.samples.dementia.settings as settings
 
 
 def main():
     """Inserts the chunks to the database."""
+    common.init_settings()
     conn_str = common.make_local_connection_string(settings.DBNAME)
     dbutil.SimpleSQL.register_connection_string(conn_str)
 
-    counter = 0
+    ragger = rag_mgr.RagManager(settings.RAG_COLLECTION_NAME)
+
     with dbutil.SimpleSQL() as db:
-        directory = settings.DIRECTORY
-        for fullpath in chunks_mgr.find_documents_to_chunk(db, directory):
-            counter += 1
-            print(datetime.datetime.now(), counter, fullpath)
-            chunks_mgr.save_chunks_to_db(db, fullpath)
+        count = ragger.insert_chunks_to_db(db, verbose=True)
+    print(f"Inserted {count} new chunks.")
 
 
 if __name__ == '__main__':
