54d3e605cbf2079f7c23be91b6a5ee5b6dbb2555
John Pazarzis <jpazarzis@gmail.com>
2024-08-19 20:21:48 -0400

    Improves the psql schema.

--- mygenai/db/create_schema.sql
+++ mygenai/db/create_schema.sql
@@ -1,3 +1,6 @@
+-- The db schema to store embeddings and keep queries.
+
+
 CREATE TABLE chunks
 (
     chunk_id      SERIAL PRIMARY KEY,
@@ -13,16 +16,16 @@ CREATE TABLE chunks
 CREATE INDEX idx_stored_in_vdb ON chunks (stored_in_vdb);
 
 
-CREATE TABLE user_msg
-{
-    user_msg_id         UUID PRIMARY KEY DEFAULT uuid_generate_v4()
-    user_id             INTEGER,
-    received_at         TIMESTAMP,
-    message             TEXT NOT NULL,
-    response            TEXT DEFAULT NULL,
-    responded_at        TIMESTAMP DEFAULT NULL,
-    thumps_up           BOOL DEFAULT NULL,
-    thumped_up_at       TIMESTAMP DEFAULT NULL
-};
+CREATE TABLE query
+(
+    query_id            uuid PRIMARY KEY DEFAULT get_random_uuid(),
+    user_id             uuid,    -- The user who made the query.
+    received_at         TIMESTAMP, -- When the query was received
+    question            TEXT NOT NULL, -- The question / message of the query
+    response            TEXT DEFAULT NULL, -- The reponse retrieved from the LLM
+    responded_at        TIMESTAMP DEFAULT NULL, -- The time the response was received
+    thumps_up           BOOL DEFAULT NULL, -- NULL no user action, True / False thumps up/down.
+    thumped_up_at       TIMESTAMP DEFAULT NULL --When it was thumped up/down
+);
 
-CREATE INDEX idx_user_msg_by_user ON user_msg (user_id);
+CREATE INDEX idx_query_by_user ON query (user_id);
