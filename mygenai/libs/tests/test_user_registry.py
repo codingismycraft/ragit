0be3f419a738d0e6cf8b1b126ed4a343bce316d2
John Pazarzis <jpazarzis@gmail.com>
2024-08-21 20:31:30 -0400

    Adds the user_registry.py.

---
+++ mygenai/libs/tests/test_user_registry.py
@@ -0,0 +1,76 @@
+"""Tests the User Registry module."""
+
+import unittest
+
+import mygenai.libs.common as common
+import mygenai.libs.user_registry as user_registry
+
+# Aliases.
+UserRegistry = user_registry.UserRegistry
+
+
+class TestUserRegistry(unittest.TestCase):
+    """Tests the user registry."""
+
+    def setUp(self):
+        """Set the root directory for the registry db."""
+        base_dir = common.get_testing_output_dir("sqlite_db", wipe_out=True)
+        UserRegistry.set_base_dir(base_dir)
+
+    def test_user_registry(self):
+        """Tests inserting a new user."""
+        UserRegistry.create_db()
+        user_name = "john"
+        email_address = "someone@someserver.com"
+        password = "mypassword"
+        UserRegistry.save(user_name, email_address, password)
+
+        user_name_1 = "someone else"
+        email_address_1 = "someoneelse@someserver.com"
+
+        # Try inserting same name.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save(user_name, email_address_1, password)
+
+        # Try inserting same email.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save(user_name_1, email_address, password)
+
+        # Try inserting very long user name.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save(user_name * 30, email_address_1, password)
+
+        # Try inserting very long email address.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save(user_name_1, email_address * 50, password)
+
+        # Try inserting very long password.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save(user_name_1, email_address_1, password * 50)
+
+        # Try getting the email for existing user.
+        retrieved_email = UserRegistry.get_email_address(user_name)
+        self.assertEqual(retrieved_email, email_address)
+
+        # Try getting the email for non-existing user.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.get_email_address("junk-user")
+
+        # Check valid password.
+        UserRegistry.validate_password(user_name, password)
+
+        # Check invalid password.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.validate_password(user_name, "junk")
+
+        # Validate password for non existing user.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.validate_password("junk", "junk")
+
+        # Try invalid email.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.save("new_user", "invalid-email", password)
+
+        # Set invalid base directory.
+        with self.assertRaises(common.MyGenAIException):
+            UserRegistry.set_base_dir("junk")
---
+++ mygenai/libs/user_registry.py
@@ -0,0 +1,195 @@
+"""Exposes the UserRegistry static class."""
+
+import os
+import re
+
+import bcrypt
+import sqlite3
+
+import mygenai.libs.common as common
+
+
+class UserRegistry:
+    """Manages the user registry.
+
+    The user registry is meant to be used from the RagIt front-end web
+    application to hold the information for the users of the service.
+
+    To keep things easier to manage the data are stored in sqlite instead of
+    the postgres that is used for the heavy backend operations. Note that the
+    postgres database is never deployed to the front-end servers both to keep
+    the administrative tasks simpler but to also keep the required disk size
+    small.
+
+    The user registry is self-sufficient meaning that the database is created
+    automatically by the code if needed under the well known shared directory.
+
+    :cvar str _base_dir: The parent directory holding the sqlite db file. By
+    default the shared directory will be used.
+    """
+    _DB_FILENAME = "user_registry.sqlite.db"
+    _MAX_USER_NAME_LENGTH = 32
+    _MAX_EMAIL_ADDRESS_LENGTH = 64
+    _MAX_PASSWORD_LENGTH = 32
+
+    _EMAIL_VALIDATOR = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+    _SQL_CREATE_TABLE = """
+        CREATE TABLE users (
+                    user_name TEXT UNIQUE NOT NULL,
+                    email     TEXT UNIQUE NOT NULL,
+                    hashed_password  BLOB NOT NULL
+                )
+    """
+
+    _SQL_INSERT_USER = """
+    INSERT INTO users (user_name, email, hashed_password)
+    VALUES (?, ?, ?);
+    """
+
+    _SQL_SELECT_EMAIL = """SELECT email from users where user_name=? """
+
+    _SQL_SELECT_PASSWD = """
+        SELECT hashed_password from users where user_name=?
+    """
+
+    _base_dir = None  # By default, will be the shared directory.
+
+    @classmethod
+    @common.handle_exceptions
+    def save(cls, user_name, email_address, password):
+        """Saves the info for the passed in user to the database.
+
+        :param str user_name: The name of the user to save, it must be unique.
+
+        :param str email_address: The email address of the user to save, it
+        must be unique.
+
+        :param str password: The password for the user to save.
+
+        :raises: MyGenAIException
+        """
+        user_name = user_name.strip()
+        email_address = email_address.strip()
+        password = password.strip()
+
+        if len(user_name) >= cls._MAX_USER_NAME_LENGTH:
+            raise ValueError("Too long User name")
+
+        if len(email_address) >= cls._MAX_EMAIL_ADDRESS_LENGTH:
+            raise ValueError("Too long email address")
+
+        cls._validate_email(email_address)
+
+        if len(password) >= cls._MAX_PASSWORD_LENGTH:
+            raise ValueError("Too long password")
+
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        conn = sqlite3.connect(cls._get_full_path())
+        cursor = conn.cursor()
+        cursor.execute(
+            cls._SQL_INSERT_USER,
+            (user_name, email_address, hashed_password)
+        )
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    @common.handle_exceptions
+    def validate_password(cls, user_name, password):
+        """Validates the passed in password.
+
+        :param str user_name: The user to validate.
+        :param str password: The password to validate.
+
+        :raises: MyGenAIException
+        """
+        with sqlite3.connect(cls._get_full_path()) as conn:
+            try:
+                cursor = conn.cursor()
+                for row in cursor.execute(cls._SQL_SELECT_PASSWD, (user_name,)):
+                    hashed_passwd = row[0]
+                    is_valid = bcrypt.checkpw(
+                        password.encode('utf-8'), hashed_passwd
+                    )
+                    if not is_valid:
+                        raise ConnectionRefusedError
+                    else:
+                        return
+            finally:
+                cursor.close()
+        raise ValueError(f"User {user_name} not found.")
+
+    @classmethod
+    @common.handle_exceptions
+    def get_email_address(cls, user_name):
+        """Returns the email address for the passed in user.
+
+        :param str user_name: The user to return the email for.
+
+        :returns: The email for the passed in user.
+
+        :raises: MyGenAIException
+        """
+        with sqlite3.connect(cls._get_full_path()) as conn:
+            try:
+                cursor = conn.cursor()
+                for row in cursor.execute(cls._SQL_SELECT_EMAIL, (user_name,)):
+                    return row[0]
+            finally:
+                cursor.close()
+        raise ValueError(f"User {user_name} not found.")
+
+    @classmethod
+    @common.handle_exceptions
+    def create_db(cls):
+        """Creates the database file if it does not exist.
+
+        :raises: MyGenAIException
+        """
+        fullpath = cls._get_full_path()
+        if os.path.exists(fullpath):
+            raise FileExistsError
+        conn = sqlite3.connect(fullpath)
+        cursor = conn.cursor()
+        cursor.execute(cls._SQL_CREATE_TABLE)
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    @common.handle_exceptions
+    def set_base_dir(cls, base_dir):
+        """Allows to customize the directory where the sqlite db is held.
+
+        :param str base_dir: The directory where the sqlite db is held; pass
+        None to use the default location which points to the shared directory.
+
+        :raises: MyGenAIException
+        """
+        if not os.path.isdir(base_dir):
+            raise NotADirectoryError
+        cls._base_dir = base_dir
+
+    @classmethod
+    def _get_full_path(cls):
+        """Returns the full path to the sqlite db file.
+
+        :return: The full path to the sqlite db file.
+        :rtype: str
+        """
+        base_dir = cls._base_dir or common.get_shared_directory()
+        fullpath = os.path.join(base_dir, cls._DB_FILENAME)
+        return fullpath
+
+    @classmethod
+    def _validate_email(cls, email_address):
+        """Validates the passed in email address.
+
+        :param str email_address: The email address to validate.
+
+        :raises: ValueError if the passed in email address is invalid.
+        """
+        if not re.fullmatch(cls._EMAIL_VALIDATOR, email_address):
+            raise ValueError("Invalid email.")
